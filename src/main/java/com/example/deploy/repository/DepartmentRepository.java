2ce8acf4da5648076e6ac4785bf15d785dd1c40d
woogi123 <07491@naver.com>
2025-07-02 09:55:26 +0900

    runner, Entity, Repository 추가

---
+++ src/main/java/com/example/deploy/repository/DepartmentRepository.java
@@ -0,0 +1,8 @@
+package com.example.deploy.repository;
+
+import com.example.deploy.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+}
